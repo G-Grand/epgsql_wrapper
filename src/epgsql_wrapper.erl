7e34e17b8d1576353aa84a4d6eacedf963cf5aad
Max Davidenko <armwebdev@gmail.com>
2015-01-21 17:53:25 +0300

    Simple query function implemented

--- src/epgsql_wrapper.erl
+++ src/epgsql_wrapper.erl
@@ -5,12 +5,31 @@
 %% Application callbacks
 -export([start/2, stop/1]).
 
+%% API
+-export([simpleQuery/1]).
+
 %% ===================================================================
 %% Application callbacks
 %% ===================================================================
 
 start(_StartType, _StartArgs) ->
-    epgsql_wrapper_sup:start_link().
+    lager:start(),
+    Host = application:get_env(epgsql_wrapper, host, "localhost"),
+    Port = application:get_env(epgsql_wrapper, port, 5432),
+    User = application:get_env(epgsql_wrapper, user, "user"),
+    Pass = application:get_env(epgsql_wrapper, pass, "pass"),
+    Db = application:get_env(epgsql_wrapper, db, "test"),
+    Timeout = application:get_env(epgsql_wrapper, timeout, 5000),
+    Args = [{host, Host}, {port, Port}, {user, User}, {pass, Pass}, {db, Db}, {timeout, Timeout}],
+    epgsql_wrapper_sup:start_link([Args]).
 
 stop(_State) ->
+    lager:stop(),
     ok.
+
+%% ===================================================================
+%% API
+%% ===================================================================
+
+simpleQuery(SQL) ->
+    epgsql_wrapper_worker:simpleQuery(SQL).
--- src/epgsql_wrapper_sup.erl
+++ src/epgsql_wrapper_sup.erl
@@ -3,25 +3,29 @@
 -behaviour(supervisor).
 
 %% API
--export([start_link/0]).
+-export([start_link/1]).
 
 %% Supervisor callbacks
 -export([init/1]).
 
 %% Helper macro for declaring children of supervisor
 -define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
+-define(CHILD_WITH_ARGS(I, Type, Args), {I, {I, start_link, Args}, transient, 5000, Type, [I]}).
 
 %% ===================================================================
 %% API functions
 %% ===================================================================
 
-start_link() ->
-    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+start_link(Args) ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, Args).
 
 %% ===================================================================
 %% Supervisor callbacks
 %% ===================================================================
 
-init([]) ->
-    {ok, { {one_for_one, 5, 10}, []} }.
+init(Args) ->
+    ConnWorker = ?CHILD_WITH_ARGS(epgsql_wrapper_worker, worker, Args),
+    RestartStrategy = {one_for_one, 5, 60},
+    Childs = [ConnWorker],
+    {ok, { RestartStrategy, Childs} }.
 
